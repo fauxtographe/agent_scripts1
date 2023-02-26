f11821f2b0ba1d9ddca9c3047242a4f00c285b03
vagrant <vagrant@localhost.localdomain>
2023-02-26 21:26:20 +0000

    new script

---
+++ sonar-install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Script by Casey, Feb 2023
+
+#Change user to vagrant
+su - vagrant
+
+#Install Java 11
+sudo yum update -y
+sudo yum install java-11-openjdk-devel -y
+sudo yum install java-11-openjdk -y
+
+#Install wget
+sudo yum install wget -y
+
+#Download SonarQube
+cd /opt
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip
+
+#Install unzip
+sudo yum install unzip -y
+
+#Unzip SonarQube
+sudo unzip /opt/sonarqube-9.3.0.51899.zip
+
+#Change permissions
+sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
+
+#Start service
+cd /opt/sonarqube-x.x/bin/linux-x86-64 
+./sonar.sh start
+
